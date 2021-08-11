c82811610a7b21bdd06780341b2848105627a597
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-08-11 09:59:25 -0300

    Adicionando fluxo de chefia solicitar GRU

--- cypress/integration/chefia.feature
+++ cypress/integration/chefia.feature
@@ -5,15 +5,24 @@ Eu como parte da chefia, desejo analisar nota tecnica ou designar um analista
 
 Contexto:
     Dado Que o usuário acesse o sistema com o perfil "Chefia ICMBio/Responsável Designado"
-    E Que exista um processo na etapa "Análise de conformidade"
 
     Cenario: Designar Analista
-    Dado Que o usuário selecione a opção "Designar Analista"
+    Dado Que exista um processo na etapa "Análise de conformidade"
+    E Que o usuário selecione a opção "Designar Analista"
     E Designe o analista "Aline Carla"
     Entao O Processo deve ser designado
 
     Cenario: Analisar Nota Tecnica
-    Dado Que o usuário selecione a opção "Gerar nota técnica"
+    Dado Que exista um processo na etapa "Análise de conformidade"
+    E Que o usuário selecione a opção "Gerar nota técnica"
     E O usuário solicitar assinar e enviar
     Quando O usuário enviar os dados ao SEI
     Entao A nota técnica deve ser salva
+
+    Cenario: Solicitar Cobrança de GRU
+    Dado Que exista um processo na etapa "Análise de conformidade realizada"
+    E Que o usuário selecione a opção "Encaminhar Posicionamento"
+    Quando O usuário marcar "Sim" para a emissão de GRU
+    E O usuário anexe o documento na GRU
+    E O usuário solicite salvar
+    Então Os dados da GRU devem ser salvos
--- cypress/integration/chefia/steps.js
+++ cypress/integration/chefia/steps.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 
-When(/^Que exista um processo na etapa "([^"]*)"$/, (etapa) => {
+Given(/^Que exista um processo na etapa "([^"]*)"$/, (etapa) => {
     cy.get('input[type="search"]').type(etapa)
     cy.wait(4000)
 	cy.get('td').contains(new RegExp('^' + etapa + '$', 'g'))
 });
 
-Given(/^Que o usuário selecione a opção "([^"]*)"$/, opcao => {
+When(/^Que o usuário selecione a opção "([^"]*)"$/, opcao => {
 	cy.get('[title=\"'+opcao+'\"]').first().click()
 });
 
@@ -34,4 +34,28 @@ When(/^O usuário solicitar assinar e enviar$/, () => {
 
 Then(/^A nota técnica deve ser salva$/, () => {
 	cy.wait('@gerarNotaTecnicaSei').its('response.body').should('include', 'Nota t\\u00e9cnica salva com sucesso.')
+});
+
+When(/^O usuário marcar "([^"]*)" para a emissão de GRU$/, (opt) => {
+    cy.wait(1500)
+    cy.contains(opt).siblings('input').click()
+});
+
+When(/^O usuário anexe o documento na GRU$/, () => {
+	cy.fixture('SOALA_GRU_Template.pdf').then(conteudo => {
+        cy.get('input[type="file"]').attachFile({
+            fileContent: conteudo.toString(),
+            fileName: 'SOALA_GRU_Template.pdf',
+        })
+    })
+});
+
+When(/^O usuário solicite salvar$/, () => {
+    cy.intercept('POST', 'https://tctisoala.sisicmbio.icmbio.gov.br/designacao/chefia/salvarArquivoGRU')
+    .as('salvarGRU')
+	cy.contains('Salvar').click()
+});
+
+Then(/^Os dados da GRU devem ser salvos$/, () => {
+	cy.wait('@salvarGRU').its('response.body').should('include', 'Os dados foram salvos com sucesso.')
 });
