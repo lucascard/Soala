82bbc4c6dac3fdf983966c5a46c1569d69049bb1
Eduardo Afonso <oeduardoafonso@gmail.com>
2021-08-13 10:17:57 -0300

    Refatorando fluxo de coordenador

--- cypress/integration/coordenadorGerente.feature
+++ cypress/integration/coordenadorGerente.feature
@@ -1,35 +1,35 @@
 #language: pt
 Funcionalidade: Coordenador/Gerente
 
-    Como Coordenador, desejo entrar na página do SOALA
+    Como Coordenador, desejo designar equipe, gerar nota técnica e gerar despacho de análise de conformidade.
+    
     Contexto:
     Dado Que o usuário acesse o sistema com o perfil "Coordenador/Gerente"
     # Quando que um novo processo seja criado
     
     Cenario: Coordenador/Gerente designar designador
-    
-    Entao Eu entro na página MEUS PROCESSOS
-
-    Dado Que eu clique em pesquisar e digite MANIFESTAÇÃO DE PARECERISTA
-    E Clique no primeiro botão de DESIGNAR EQUIPE
-    Entao Eu entro na página DESGINAR EQUIPE
-
-    Dado Que eu selecione o perfil responsável DESIGNADOR
-    E Selecione a Unidade Afetada APA Anhatomirim...
-    E Selecione a designadora Aline Carla Gonçalves
-    Quando Eu clico em salvar
-    Entao Aparece uma mensagem de confirmação.
-
-    Dado Que eu clique em Gerar Despacho
-    E Aperte em Sim
-    E Entre na página Gerar Despacho
-    Quando Eu selecionar Assinar Documento
-    Entao Uma modal é aberta
-
-    #Modal SEI
+    Dado Que exista um processo na etapa "Manifestação de parecerista"
+    E Que o usuário selecione a opção "Designar equipe"
+    Quando O usuário informar os dados:
+    |   Designar    |   Unidade_Afetada |   Responsavel               |
+    |   Designador  |   APA Anhatomirim |   Aline Carla GonÇalves     |
+    E Que o usuário selecione a opção "Gerar Despacho"
+    E Que o usuário confirme que deseja continuar
+    E O usuário solicitar assinar o despacho de designação
     E O usuário enviar os dados ao SEI
-    Entao O documento é assinado
-    E Aparece uma mensagem de confirmação.
+    Então O despacho deve ser salvo
+
+    Cenario: Coordenador/Gerente designar parecerista
+    Dado Que exista um processo na etapa "Manifestação de parecerista"
+    E Que o usuário selecione a opção "Designar equipe"
+    Quando O usuário informar os dados:
+    |   Designar    |   Unidade_Afetada |   Responsavel               |
+    |   Parecerista  |   APA Anhatomirim |   Aline Carla GonÇalves     |
+    E Que o usuário selecione a opção "Gerar Despacho"
+    E Que o usuário confirme que deseja continuar
+    E O usuário solicitar assinar o despacho de designação
+    E O usuário enviar os dados ao SEI
+    Então O despacho deve ser salvo
 
     Cenário: Coordenador/Gerente gerar nota técnica de posicionamento
     Dado Que exista um processo na etapa "Posicionamento Coordenador/Gerente"
--- cypress/integration/coordenadorGerente/steps.js
+++ cypress/integration/coordenadorGerente/steps.js
@@ -1,99 +1,42 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-import Login from '../pageObjects/login'
-import Soala from '../pageObjects/soala';
+/// <reference types="cypress" />
 
-const login = new Login()
-const soala = new Soala()
 
-Then(/^Eu entro na página MEUS PROCESSOS$/, () => {
-	cy.contains('Meus processos').should('be.visible')
-});
+When(/^O usuário informar os dados:$/, (tabelaDados) => {
+	const DADOS = tabelaDados.hashes()[0]
+	cy.wait(500)
+	cy.get('select[id="sqPerfilResponsavel"]').select(DADOS.Designar)
+	cy.wait(500)
 
-Given(/^Que eu clique em pesquisar e digite MANIFESTAÇÃO DE PARECERISTA$/, () => {
-	soala.campoPesquisar().type('Manifestação de parecerista')
-});
+	if(DADOS.Designar === 'Parecerista') cy.get('button[data-id="sqUnidadesAfetadas"]').click()
+	else cy.get('button[data-id="sqUnidadeAfetada"]').click()
+	
+	cy.wait(500)
+	cy.get('div[class="bs-searchbox"] input:visible').type(DADOS.Unidade_Afetada)
+	cy.wait(500)
+	cy.contains('span', DADOS.Unidade_Afetada).click()
+	cy.get('.card-header').click()
 
-When(/^Clique no primeiro botão de DESIGNAR EQUIPE$/, () => {
-	soala.primeiroBotaoDesignar().click()
-});
-
-Then(/^Eu entro na página DESGINAR EQUIPE$/, () => {
-	cy.contains('Designar Equipe').should('be.visible')
-});
-
-Given(/^Que eu selecione o perfil responsável DESIGNADOR$/, () => {
+	if(DADOS.Designar === 'Parecerista') cy.get('button[data-id="sqPessoasResponsaveis"]').click()
+	else cy.get('button[data-id="sqPessoaResponsavel"]').click()
+	
+	cy.wait(500)
+	cy.get('div[class="bs-searchbox"] input:visible').type(DADOS.Responsavel)
 	cy.wait(1500)
- 	soala.perfilResponsavel().select('Designador')
+	cy.contains('span[class="text"]', DADOS.Responsavel).click()
+	cy.get('.card-header').click()
+	cy.contains('Salvar').click()
 });
 
-When(/^Selecione a Unidade Afetada APA Anhatomirim...$/, () => {
-	soala.unidadeAfetada().type('APA Anhatomirim')
+When(/^Que o usuário confirme que deseja continuar$/, () => {
 	cy.wait(1000)
-	soala.primeiraUnidadeAfetada().click()
-});
+	cy.contains('button', 'Sim').click()
+}); 
 
-When(/^Selecione a designadora Aline Carla Gonçalves$/, () => {
-	soala.informarDesignador().type('Aline Carla Gonçalves')
-	soala.primeiroDesignador().click()	
-});
-
-When(/^Eu clico em salvar$/, () => {
-	soala.botaoSalvarDesignacao().click()
-});
-
-Then(/^Aparece uma mensagem de confirmação.$/, () => {
-	return true
-});
-
-Given(/^Que eu clique em Gerar Despacho$/, () => {
-	
-	//cy.get('[class="col-sm-12"]').contains('APA Anhatomirim').parents('tr[id="row-432"]').find('span[title="Gerar Despacho"]').click()
-	cy.get('[class="col-sm-12"]').contains('APA Anhatomirim').siblings('td').find('span[title="Gerar Despacho"]').click()
-	
-	
-});
-
-When(/^Aperte em Sim$/, () => {
-	cy.get('.swal2-confirm').click()
-});
-
-When(/^Entre na página Gerar Despacho$/, () => {
-	cy.contains('Gerar Despacho').should('be.visible')
-});
-
-When(/^Eu selecionar Assinar Documento$/, () => {
-	cy.wait(2000)
-	cy.get('.card-body > .btn').click()
-});
-
-Then(/^Uma modal é aberta$/, () => {
-	return true
-});
-
-Given(/^Que eu esteja na modal Dados de Envio ao SEI$/, () => {
-	cy.contains('Dados de Envio ao SEI').should('be.visible')
-});
-
-Then(/^Preencha os campos de Login e senha$/, () => {
-	cy.get('#userLogin').type('lucas')
-	cy.get('#userSenha').type('lucas123')
-	
-});
-
-Then(/^Selecione um cargo$/, () => {
-	cy.get('#userCargo').select('Assessor(a)')
-});
-
-When(/^Eu clicar em enviar$/, () => {
-	cy.get('.btn-send').click()
-});
-
-Then(/^O documento é assinado$/, () => {
-	return true;
-});
-
-Then(/^Aparece uma mensagem de confirmação.$/, () => {
-	cy.contains('Despacho Interlocutório cadastrado com sucesso.').should('be.visible')
+When(/^O usuário solicitar assinar o despacho de designação$/, () => {
+    cy.wait(300)
+	cy.intercept('POST', 'https://tctisoala.sisicmbio.icmbio.gov.br/designacao/saveDespacho')
+    .as('salvarDespacho')
+	cy.contains('Assinar Documento').click()
 });
 
 When(/^O usuário solicitar assinar o documento$/, () => {
